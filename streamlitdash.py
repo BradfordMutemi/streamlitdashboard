6e96453d4672cc6044e5c16ce388ebfe2ba42840
Bradford Mutemi <bradmutemi@gmail.com>
2023-11-08 20:06:28 +0300

    Current Commit

--- streamlitdash.py
+++ streamlitdash.py
@@ -15,8 +15,40 @@ fl = st.file_uploader(":file_folder: Upload a file", type=(["csv", "txt", "xlsx"
 if fl is not None:
     filename = fl.name
     st.write(filename)
-    df = pd.read_csv(filename)
+    df = pd.read_csv(filename, encoding="ISO-8859-1")
 else:
     os.chdir(r"C:\Users\bradd\Documents\DATA SCIENCE\PROJECTS\streamlitdashboard")
-    df = pd.read_csv("SampleSuperstore.csv")
+    df = pd.read_csv("SampleSuperstore.csv", encoding="ISO-8859-1")
+col1, col2 = st.columns((2))
+df['Order Date'] = pd.to_datetime(df['Order Date'], format="mixed")
 
+# Getting min and max date
+startDate = pd.to_datetime(df['Order Date']).min()
+endDate = pd.to_datetime(df['Order Date']).max()
+
+with col1:
+    date1 = pd.to_datetime(st.date_input("Order Date", startDate))
+
+with col2:
+    date2 = pd.to_datetime(st.date_input("Order Date", endDate))
+
+df = df[(df["Order Date"] >= date1) & (df["Order Date"] <= date2)].copy()
+
+# Region Filter
+st.sidebar.header("Choose Your Filter:")
+region = st.sidebar.multiselect("Pick the Region", df["Region"].unique())
+
+if not region:
+    df2 = df.copy()
+else:
+    df2 = df[df["Region"].isin(region)]
+
+# State Filter
+state = st.sidebar.multiselect("Pick the State", df2["State"].unique())
+if not state:
+    df3 = df2.copy()
+else:
+    df3 = df2[df2["State"].isin(state)]
+
+# City Filter
+city = st.sidebar.multiselect("Pick the City", df3["City"].unique())
